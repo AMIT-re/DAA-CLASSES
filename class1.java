51e6270e3abc1b110c457ef651e322c1f35b62db
AMIT-re <amitkumarsingh21feb2005@gmail.com>
2025-04-03 12:53:22 +0530

    class sem 4

--- class1.java
+++ class1.java
@@ -475,19 +475,13 @@ class class1 {
         
         int n1 = m - l + 1;
         int n2 = r - m;
-
         int L[] = new int[n1];
         int R[] = new int[n2];
-
-
         for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
-
-
         int i = 0, j = 0;
-
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
@@ -500,14 +494,11 @@ class class1 {
             }
             k++;
         }
-
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
-
-
         while (j < n2) {
             arr[k] = R[j];
             j++;
@@ -515,22 +506,16 @@ class class1 {
         }
     }
 
-
     static void sort(int arr[], int l, int r)
     {
         if (l < r) {
 
-
             int m = l + (r - l) / 2;
-
-
             sort(arr, l, m);
             sort(arr, m + 1, r);
-
             merge(arr, l, m, r);
         }
     }
-
 
     static void printArray(int arr[])
     {
