360c15dc9076e1ce5b8d1b0535eccab9fe930720
AMIT-re <amitkumarsingh21feb2005@gmail.com>
2025-04-10 19:26:48 +0530

    new

---
+++ strfucn.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class strfucn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the string: ");
+        String s = sc.nextLine();
+        //String s = "abc";
+        // int a =0;
+        // int b = 0;
+        // int c =0;
+        // for(int i = 0 ; i<s.length(); i++){
+        //     if(s.charAt(i)=='a'){
+        //         a++;
+        //     }else if(s.charAt(i)=='b'){
+        //         b++;
+        //     }else if(s.charAt(i)=='c'){
+        //         c++;
+        //     }
+        // }
+        // int max = Math.max(a, Math.max(b, c));
+        // int result = max *3-s.length();
+        // System.out.println("Result: " + result);
+        int i = 0;
+        char ch = 97;
+        int count = 0;
+        while(i<s.length()){
+            if(s.charAt(i) != ch){
+                count++;
+                ch++;
+            }else{
+                i++;
+                ch++;
+            }
+            if(ch == 100){
+                ch = 97;
+            }
+        }
+        System.out.println("Result: " + count);
+        
+}
+}
