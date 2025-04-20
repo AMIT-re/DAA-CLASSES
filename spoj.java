fd770a834a12fd14e4f01c96df4ee5d7a584817a
AMIT-re <amitkumarsingh21feb2005@gmail.com>
2025-04-20 15:39:49 +0530

    20 april

---
+++ spoj.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+
+class spoj
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int temp=sc.nextInt();
+        while(temp!=42)
+        {
+            System.out.println(temp);
+            temp=sc.nextInt();    
+        }
+		
+		
+		
+	}
+}
+
