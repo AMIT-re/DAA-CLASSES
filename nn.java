e664be79ad8b93dabd56971ac4a6e14ef09e9a51
AMIT-re <amitkumarsingh21feb2005@gmail.com>
2025-04-23 20:40:59 +0530

    23 april

---
+++ nn.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class nn {
+    public static void main(String[] args){
+
+    
+    int arr[] ={1,2,2,3,3,4,5,6};
+    // int smallest=arr[0];
+    // int ss=Integer.MAX_VALUE;
+    // for(int i=0;i<arr.length;i++)
+    // {
+    //     if(arr[i]<smallest)
+    //     {
+    //         ss=smallest;
+    //         smallest=arr[i];
+    //     }
+    //     if(arr[i]<ss && arr[i]!=smallest)
+    //     {
+    //         ss=arr[i];
+    //     }   
+    // }
+    // System.out.println(ss);
+
+    // HashMap<Integer,Integer> hm=new HashMap<>();
+    // for(int i=0;i<arr.length;i++)
+    // {   
+    //     hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+    // }
+    // int temp[]=new int[hm.size()];
+    // for(int i=0;i<hm.size();i++)
+    // {
+    //     temp[i]=hm.get(i);  
+    // }
+    // System.out.println(Arrays.toString(temp));
+    // String str="12xyz34";
+    // for(int i=0;i<str.length();i++)
+    // {
+    //     char ch=str.charAt(i);
+    //     if(ch>=49 && ch<=58)
+    //     {
+            
+    //     }
+    // }
+}
+}
+
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,28 +1,9 @@
-public class second_largest {
-    public static void main(String[]  args)
-    {
-        int[] arr={1,2,3,4,5,6,90};
-        int max=0;
-        int smax=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]>max)
-            {
-                smax=max;
-                max=arr[i];
-            }
-            else if(max!=smax &&arr[i]>smax){
-                smax=arr[i];
-
-            }
-        }
-        if(smax==max)
-        {
-            System.out.println("-1");
-        }
-        else{
-            System.out.println(smax);
-        }
-    }
-    
-}
+    // String str="12xyz34";
+    // for(int i=0;i<str.length();i++)
+    // {
+    //     char ch=str.charAt(i);
+    //     if(ch>=49 && ch<=58)
+    //     {
+            
+    //     }
+    // }
