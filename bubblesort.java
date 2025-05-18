d1a1fd85de3eec9e241bc18b436e8a60c2359b72
AMIT-re <amitkumarsingh21feb2005@gmail.com>
2025-05-18 17:47:27 +0530

    18th may

---
+++ bubblesort.java
@@ -0,0 +1,24 @@
+public class bubblesort {
+    public static void main(String[] main)
+    {
+        int arr[]={9,8,7,6,4,3,2,1};
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=0;j<arr.length-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+    }
+    
+}
